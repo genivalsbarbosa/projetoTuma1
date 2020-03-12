b22ccd567252d804268f97a111cb7133aa618b3e
Genival Soares Barbosa Filho <gbarbosa@procenge.com.br>
2020-03-12 11:40:20 -0300

    first commit

---
+++ src/app/curso/curso.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Curso } from './servico/curso';
+import { CursoService } from './servico/curso.service';
+
+@Component({
+  selector: 'app-curso',
+  templateUrl: './curso.component.html',
+  styleUrls: ['./curso.component.scss']
+})
+export class CursoComponent implements OnInit {
+
+
+  curso: Curso = new Curso();
+
+  constructor(
+    private cursoServico: CursoService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  pesquisar(){
+
+    this.cursoServico.pesquisar(this.curso.nome).subscribe(
+      retorno => {
+        console.log(retorno);
+      }
+
+    );
+
+  }
+
+}
